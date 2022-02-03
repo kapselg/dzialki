3823d0f12e856201ff0c4973ae2c831a9be1b7d5
kapselgit <wojtekd2000@gmail.com>
2022-02-03 23:40:20 +0100

    working on getting the claim from db

--- src/main/java/me/kapsel/easyclaim/ClaimsContainer.java
+++ src/main/java/me/kapsel/easyclaim/ClaimsContainer.java
@@ -1,11 +1,82 @@
 package me.kapsel.easyclaim;
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 public class ClaimsContainer {
     List<EasyClaim> claims;
     public ClaimsContainer(){
         claims = new ArrayList<>();
+    }
+
+    public boolean create(EasyClaim claim){
+        if(claim.p.hasPermission("EasyClaim.create")){
+            Inventory inv = claim.p.getInventory();
+            List<ItemStack> missing = new ArrayList<>();
+            for(ItemStack item : claim.requiredItems) {
+                if (!inv.containsAtLeast(item, item.getAmount())) {
+                    //count amount left to be colected
+                    int itemsLeft = 0;
+                    for (ItemStack invStack : inv.all(item.getType()).values()) {
+                        itemsLeft += invStack.getAmount();
+                    }
+                    missing.add(new ItemStack(item.getType(), item.getAmount() - itemsLeft));
+
+                }
+            }
+            if(!missing.isEmpty()){
+                Languages.noReqItems(claim.p, missing);
+                return false;
+            }
+            //remove required items from player's inventory
+            for(ItemStack item: claim.requiredItems){
+                inv.removeItem(item);
+            }
+
+            RegionManager regions = WorldGuard.getInstance().getPlatform().getRegionContainer().get(BukkitAdapter.adapt(claim.p.getWorld()));
+
+            //checking if the flag does intersect with other claims
+            for (ProtectedRegion intersectingRegion : claim.region.getIntersectingRegions(regions.getRegions().values())){
+                if(intersectingRegion.getFlag(Main.EASY_CLAIM) == StateFlag.State.ALLOW || intersectingRegion.getFlag(Main.EASY_CLAIM_ALLOW) == StateFlag.State.DENY){
+                    Languages.intersection(claim.p);
+                    regions.removeRegion(claim.region.getId());
+                    return false;
+                }
+            }
+
+            ClaimData.get().set(claim.p.getName() + ".location.X", claim.loc.getX());
+            ClaimData.get().set(claim.p.getName() + ".location.Y", claim.loc.getY());
+            ClaimData.get().set(claim.p.getName() + ".location.Z", claim.loc.getZ());
+            ClaimData.get().set(claim.p.getName() + ".location.yaw", claim.loc.getYaw());
+            ClaimData.get().set(claim.p.getName() + ".pt1", claim.pt1);
+            ClaimData.get().set(claim.p.getName() + ".pt2", claim.pt2);
+            ClaimData.get().set(claim.p.getName() + ".location.world", Objects.requireNonNull(claim.loc.getWorld()).getName());
+            ClaimData.get().set(claim.p.getName() + ".size", claim.offset);
+            ClaimData.get().set(claim.p.getName() + ".creation-date", new Date().toString());
+            ClaimData.get().set(claim.p.getName() + "._easy-claim-1.members", new String[]{""});
+            ClaimData.save();
+
+            regions.addRegion(claim.region);
+
+            Languages.claimCreated(claim.p);
+            return true;
+        }else{
+            Languages.noPermission(claim.p);
+            return false;
+        }
+    }
+    public boolean getClaim(Player p){
+        new EasyClaim(ClaimData.get().getList(p.getName()));
     }
 }
--- src/main/java/me/kapsel/easyclaim/EasyClaim.java
+++ src/main/java/me/kapsel/easyclaim/EasyClaim.java
@@ -9,20 +9,23 @@ import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class EasyClaim {
 
-    private Player p;
-    private ProtectedCuboidRegion region;
-    private final List<ItemStack> requiredItems;
-    private Location loc;
+    final Player p;
+    final ProtectedCuboidRegion region;
+    final List<ItemStack> requiredItems;
+    final Location loc;
+    final BlockVector3 pt1;
+    final BlockVector3 pt2;
     int offset;
 
     //class constructor
@@ -53,8 +56,8 @@ public class EasyClaim {
 
             //calculate the region size
             //BlockVector3 vec = new BlockVector3(loc.getX(), loc.getY(), loc.getZ());
-            BlockVector3 pt1 = BlockVector3.at(loc.getX() + offset/2, loc.getY() + offset/2, loc.getZ() + offset/2);
-            BlockVector3 pt2 = BlockVector3.at(loc.getX() - offset/2, loc.getY() - offset/2, loc.getZ() - offset/2);
+            pt1 = BlockVector3.at(loc.getX() + offset/2, loc.getY() + offset/2, loc.getZ() + offset/2);
+            pt2 = BlockVector3.at(loc.getX() - offset/2, loc.getY() - offset/2, loc.getZ() - offset/2);
 
             region = new ProtectedCuboidRegion(
                     p.getName() + "_easy-claim-1",
@@ -76,61 +79,12 @@ public class EasyClaim {
 
 
     }
-    public boolean create(){
-        if(p.hasPermission("EasyClaim.create")){
-            Inventory inv = p.getInventory();
-            List<ItemStack> missing = new ArrayList<>();
-            for(ItemStack item : requiredItems) {
-                if (!inv.containsAtLeast(item, item.getAmount())) {
-                    //count amount left to be colected
-                    int itemsLeft = 0;
-                    for (ItemStack invStack : inv.all(item.getType()).values()) {
-                        itemsLeft += invStack.getAmount();
-                    }
-                    missing.add(new ItemStack(item.getType(), item.getAmount() - itemsLeft));
+    public EasyClaim(String playerName){
+        p = Bukkit.getPlayer(playerName);
 
-                }
-            }
-            if(!missing.isEmpty()){
-                Languages.noReqItems(p, missing);
-                return false;
-            }
-            //remove required items from player's inventory
-            for(ItemStack item: requiredItems){
-                inv.removeItem(item);
-            }
-
-            RegionManager regions = WorldGuard.getInstance().getPlatform().getRegionContainer().get(BukkitAdapter.adapt(p.getWorld()));
-
-            //checking if the flag does intersect with other claims
-            for (ProtectedRegion intersectingRegion : region.getIntersectingRegions(regions.getRegions().values())){
-                if(intersectingRegion.getFlag(Main.EASY_CLAIM) == StateFlag.State.ALLOW || intersectingRegion.getFlag(Main.EASY_CLAIM_ALLOW) == StateFlag.State.DENY){
-                    Languages.intersection(p);
-                    regions.removeRegion(region.getId());
-                    return false;
-                }
-            }
-
-            ClaimData.get().set(p.getName() + ".location.X", loc.getX());
-            ClaimData.get().set(p.getName() + ".location.Y", loc.getY());
-            ClaimData.get().set(p.getName() + ".location.Z", loc.getZ());
-            ClaimData.get().set(p.getName() + ".location.yaw", loc.getYaw());
-            ClaimData.get().set(p.getName() + ".location.world", Objects.requireNonNull(loc.getWorld()).getName());
-            ClaimData.get().set(p.getName() + ".size", offset);
-            ClaimData.get().set(p.getName() + ".creation-date", new Date().toString());
-            ClaimData.get().set(p.getName() + "._easy-claim-1.members", new String[]{""});
-            ClaimData.save();
-
-            regions.addRegion(region);
-
-            Languages.claimCreated(p);
-            return true;
-        }else{
-            Languages.noPermission(p);
-            return false;
-        }
     }
-
+    //pushes the claim to database
+    //removes claim from db
     public boolean delete(boolean isConfirmed){
         //check if player has a claim
         ClaimData.reload();
